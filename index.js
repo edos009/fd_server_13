3f3c26e00515899075fe5b121dbc6e4e7e08b4fa
edos009 <dostyutyunnik@gmail.com>
2022-09-13 14:25:38 +0300

    Teoria

---
+++ index.js
@@ -0,0 +1,12 @@
+//module - определяет файл на который мы смотрим
+/*
+resolving -> loading -> wrraping -> evalution -> cachinh
+core
+node_modules
+-----
+filename js|json
+-----
+directory
+  package.json -> main
+  index js| json
+*/
