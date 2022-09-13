858f3ba11b928adce0885127e1bfc79db0028245
edos009 <dostyutyunnik@gmail.com>
2022-09-13 16:57:44 +0300

    Create server

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-//module - определяет файл на который мы смотрим
-/*
-resolving -> loading -> wrraping -> evalution -> cachinh
-core
-node_modules
------
-filename js|json
------
-directory
-  package.json -> main
-  index js| json
-*/
+//Подключаем сервер
+const http = require('http');
+const PORT = 3000;
+
+const requestListener = (req, res) => {
+  console.log('req', req);
+  console.log('res', res);
+  res.end("Hello I'm learning Node"); // ответ сервера
+}
+
+//Создание сервера
+const server = http.createServer(requestListener);
+server.listen(PORT); //запуск сервера | Порт 80 http / 443 https
